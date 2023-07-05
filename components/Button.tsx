49aa510e9f75efb35fcb4138d7e31b0d9d86eb2e
brigss <brigitta.rucz@yahoo.com>
2023-07-05 11:22:11 +0300

    Add customizable button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ href, title }: { href: string; title: string }) => {
+  return (
+    <Link href={href}>
+      <button className="bg-violet-800 text-violet-50 p-3 pl-10 pr-10 m-5 rounded-md shadow-lg">
+        {title}
+      </button>
+    </Link>
+  );
 };
 
 export default Button;
