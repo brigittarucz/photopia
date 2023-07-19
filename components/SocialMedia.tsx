e957f7426a81f9a3405cc190cf1da5f8eb491833
brigss <brigitta.rucz@yahoo.com>
2023-07-19 19:21:05 +0300

    Update socialMedia.tsx for md

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const SocialMedia = () => {
   return (
-    <div className="border border-gray-600 pt-1 pr-1.5 pl-1 inline-block ">
-      <div className="border border-gray-600 w-fit p-2 m-1 inline-block ">
+    <div className="border border-gray-600 pt-1 pr-1.5 pl-1 inline-block md:border-0 md:block">
+      <div className="border border-gray-600 w-fit p-2 m-1 inline-block md:bg-white md:hover:bg-slate-50 md:hover:cursor-pointer">
         <Image
           src="/assets/icons/linkedin.png"
           width="30"
@@ -12,7 +12,7 @@ const SocialMedia = () => {
           alt="LinkedIn"
         />
       </div>
-      <div className="border border-gray-600 w-fit p-2 m-1 inline-block ">
+      <div className="border border-gray-600 w-fit p-2 m-1 inline-block md:block md:bg-white md:hover:bg-slate-50 md:hover:cursor-pointer">
         <Image
           src="/assets/icons/behance.png"
           width="30"
@@ -20,7 +20,7 @@ const SocialMedia = () => {
           alt="Behance"
         />
       </div>
-      <div className="border border-gray-600 w-fit p-2 m-1 inline-block ">
+      <div className="border border-gray-600 w-fit p-2 m-1 inline-block md:bg-white md:hover:bg-slate-50 md:hover:cursor-pointer">
         <Image
           src="/assets/icons/dribbble.png"
           width="30"
