2f5a921c3f50f4a529d6a53b97df5a2bbef48fc7
brigss <brigitta.rucz@yahoo.com>
2023-07-19 19:18:56 +0300

    Add react carousel package

---
+++ components/Sessions/components/Carousel.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import Button from "@components/Button";
+import React from "react";
+import ReactCarousel from "react-carousel-lwui";
+
+const Carousel = () => {
+  return (
+    <article className="mb-5">
+      <ReactCarousel
+        images={[
+          <img
+            src="/assets/images/placeholder-puzzle-3.png"
+            alt="placeholder"
+            key=""
+          />,
+          <img
+            src="/assets/images/placeholder-puzzle-4.png"
+            alt="placeholder"
+            key=""
+          />,
+          <img src="/assets/images/cover.gif" alt="placeholder" key="" />,
+        ]}
+        header={<Button title="Buy at 125dkk" href="/" />}
+      />
+    </article>
+  );
+};
+
+export default Carousel;
